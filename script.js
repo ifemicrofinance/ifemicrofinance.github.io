482ebaa7ef13e4bd07103de381722dae3684361f
ifemicrofinance <37254532+ifemicrofinance@users.noreply.github.com>
2018-03-16 15:32:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,39 @@ $(document).ready(function(){
         window.location.hash = hash;
       });
     }
+  });$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
   });
+
+  window.onload = function() {
+         var loaded = document.getElementsByTagName("body")[0];
+         loaded.className = "loaded";
+  };
+
+  setTimeout(function() {
+    $('body').addClass('loaded');
+    $(document).scrollTop(0);
+  });
+
+  window.onscroll = function() {scroll()};
+
+  function scroll() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("bar").style.width = scrolled + "%";
+  }
+});
+
 
   window.onload = function() {
          var loaded = document.getElementsByTagName("body")[0];
@@ -35,7 +67,7 @@ $(document).ready(function(){
       var email = document.getElementById('email')
       var message = document.getElementById('message')
 
-      $.ajax({
+      $.ajax({         
           method: 'POST',
           url: '//formspree.io/microfinanceife@gmail.com',
           data: $('#contact-form').serialize(),
